564d5a2a0d5936a2b034adcdf798c751a9315b20
Andrew Short <andrews228@gmail.com>
2017-09-30 14:03:44 -0400

    changing PORT back to 8080

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var express = require('express');
 var server = express();
 var http = require('http');
 server.use('/', express.static(__dirname + '/public/'));
-server.listen(80);
+server.listen(8080);
