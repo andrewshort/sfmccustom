12bfb47f9fef9d0b0d230ce841dbec002210660d
Andrew Short <ashort@ashort-ltm.internal.salesforce.com>
2017-10-03 09:57:42 -0400

    configure urls for activity

--- public/customActivity.js
+++ public/customActivity.js
@@ -16,15 +16,35 @@ define([
         connection.trigger('ready'); // JB will respond the first time 'ready' is called with 'initActivity'
     }
 
-    function initialize (data) {
+    function initialize(data) {
         if (data) {
             payload = data;
-        }        
+        }
+
+        if (payload.configurationArguments) {
+            $("#saveUrl").val(payload.configurationArguments.save.url);
+            $("#publishUrl").val(payload.configurationArguments.publish.url);
+            $("#validaeUrl").val(payload.configurationArguments.validate.url);
+        }  else {
+            console.log('no configurationArguments in sfmccustom');
+        }
+
+        if (payload.arguments) {
+            $("#executeUrl").val(payload.arguments.execute.url);
+        } else {
+            console.log('no arguments in sfmccustom');
+        }
 
         connection.on('clickedNext', onClickedNext)
     }
 
-    function onClickedNext () {
+    function onClickedNext() {
+
+        payload.configurationArguments.save.url = $("#saveUrl").val();
+        payload.configurationArguments.publish.url = $("#publishUrl").val();
+        payload.configurationArguments.validate.url = $("#validateUrl").val();
+        payload.arguments.execute.url = $("#executeUrl").val();
+
         payload['metaData'].isConfigured = true;
         connection.trigger('updateActivity', payload);
     }
