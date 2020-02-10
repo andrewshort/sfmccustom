97db7cdada09e08349522f6ade008b15819834e5
Andrew Short <ashort@salesforce.com>
2020-02-09 20:31:34 -0500

    initial nocode activity

---
+++ public/nocode/nocode.js
@@ -0,0 +1,47 @@
+/*jshint esversion: 6 */
+define([
+    'js/postmonger'
+], function(
+    Postmonger    
+) {
+    'use strict';
+
+    var connection = new Postmonger.Session();
+    var payload = {};
+    
+    function initialize(data) {
+        if (data) {
+            payload = data;
+        }
+
+        updateButton('next', 'Done', true);
+        updateButton('back', 'Back', false);
+    }    
+
+    function onClickedNext() {     
+        payload.metaData.isConfigured = true;
+        connection.trigger('updateActivity', payload);  
+    }
+
+    function updateButton(button, text, visible) {
+        connection.trigger('updateButton', {
+            button: button,
+            visible: visible,
+            text: text
+        });
+    }
+
+    // This is for debugging locally when there is no initActivity postmonger signal
+    (function(initFn) {
+        if (!Util.inIFrame()) {
+            initFn({});
+        }
+    })(initialize);
+
+    $(window).ready(function() {
+        connection.trigger('ready'); // JB will respond the first time 'ready' is called with 'initActivity'
+    });
+
+    connection.on('initActivity', initialize);
+    connection.on('clickedNext', onClickedNext);
+});
